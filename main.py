900fe5b958a6b526d31965c1b61ad31c6603a0cc
ZZYCX <2750658623@qq.com>
2026-09-16 22:45:11 +0800

    增加日志输出时间戳

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import os
 import traceback
 import sys
+from datetime import datetime
 from config import get_configs
 from train import run_train
 
 class Tee:
     def __init__(self, *streams):
         self.streams = streams
+        self.at_line_start = True
 
     def write(self, data):
-        for stream in self.streams:
-            stream.write(data)
-            stream.flush()
+        if not data:
+            return 0
+
+        for chunk in data.splitlines(keepends=True):
+            prefix = datetime.now().strftime('[%Y-%m-%d %H:%M:%S] ') if self.at_line_start else ''
+            for stream in self.streams:
+                stream.write(prefix + chunk)
+                stream.flush()
+            self.at_line_start = chunk.endswith(('\n', '\r'))
+
+        return len(data)
 
     def flush(self):
         for stream in self.streams:
